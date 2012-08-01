b3df67ab22e6bef3bd4c73bb477b857c7d399d89
Peter Gfader <peter@gfader.com>
2012-08-01 22:04:01 +0200

    Added KataRomanNumbers solo

---
+++ Solo/KataRomanNumerals/KataRomanNumerals/ConverterTests.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using Xunit.Extensions;
+
+namespace KataRomanNumerals
+{
+	public class ConverterTests
+    {
+		readonly RomanNumerals converter = new RomanNumerals();
+			
+		[Theory]
+		[InlineData(1, "I")]
+		[InlineData(2, "II")]
+		[InlineData(3, "III")]
+		[InlineData(4, "IV")]
+		[InlineData(5, "V")]
+		[InlineData(6, "VI")]
+		[InlineData(7, "VII")]
+		[InlineData(8, "VIII")]
+		[InlineData(9, "IX")]
+		[InlineData(10, "X")]
+		[InlineData(11, "XI")]
+		[InlineData(15, "XV")]
+		[InlineData(19, "XIX")]
+		[InlineData(44, "XLIV")]
+		[InlineData(47, "XLVII")]
+		[InlineData(48, "XLVIII")]
+		[InlineData(49, "IL")]
+		[InlineData(50, "L")]
+		[InlineData(51, "LI")]
+		[InlineData(60, "LX")]
+		[InlineData(70, "LXX")]
+		[InlineData(80, "LXXX")]
+    	public void ConvertData(int input, string expected)
+		{
+			Assert.Equal(expected, converter.Convert(input));
+		}
+		
+
+    }
+}
---
+++ Solo/KataRomanNumerals/KataRomanNumerals/RomanNumerals.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace KataRomanNumerals
+{
+	public class RomanNumerals
+	{
+		public string Convert(int input)
+		{
+			string result = "";
+
+			List<Tuple<int, string>> romans =
+				new List<Tuple<int, string>>
+					{
+						new Tuple<int, string>(50, "L"),
+						new Tuple<int, string>(49, "IL"),
+						new Tuple<int, string>(40, "XL"),
+						new Tuple<int, string>(10, "X"),
+						new Tuple<int, string>(9, "IX"),
+						new Tuple<int, string>(5, "V"),
+						new Tuple<int, string>(4, "IV"),
+						new Tuple<int, string>(1, "I"),
+					};
+
+			foreach (var roman in romans)
+			{
+				int number = roman.Item1;
+
+				while (input > number - 1)
+				{
+					result += roman.Item2;
+					input = input - number;
+				}
+			}
+
+			return result;
+		}
+	}
+}
