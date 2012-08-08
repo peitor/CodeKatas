0c265ea90856091728efbbc0ed87702ecaf45607
Peter Gfader <peter@gfader.com>
2012-08-08 22:33:37 +0200

    StringCalc Kata by Roy
    http://osherove.com/tdd-kata-1/

---
+++ Solo/KataStringCalculator/StringCalcKata2012-08-08/Calculator.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace StringCalcKata2012_08_08
+{
+	public class Calculator
+	{
+		public int Add(string input)
+		{
+			if (string.IsNullOrEmpty(input))
+			{
+				return 0;
+			}
+
+			string[] separators = GetSeparators(ref input);
+
+			input = input.Replace("\n", separators[0]);
+
+			var array = input.Split(separators, StringSplitOptions.None);
+
+			var numbers = array.Select(int.Parse);
+
+			numbers = numbers.Where(x => x < 1000);
+
+			ThrowIfContainsNegativeNumbers(numbers);
+
+			return numbers.Sum();
+
+		}
+
+		private string[] GetSeparators(ref string input)
+		{
+			List<string> separators = new List<string> { "," };
+			if (input.Length > 3 && input.StartsWith("//"))
+			{
+				if (input.Contains('['))
+				{
+					input = RemoveLeadindDashes(input);
+					while (input.Contains('['))
+					{
+						separators.Add(GetSeparatorsInner(ref input));
+					}
+					input = SkipOverNewLine(input);
+				}
+				else
+				{
+					separators.Add(input[2].ToString());
+					input = input.Substring(4);
+				}
+			}
+			return separators.ToArray();
+		}
+
+		private static string SkipOverNewLine(string input)
+		{
+			return input.Substring(1);
+		}
+
+		private static string RemoveLeadindDashes(string input)
+		{
+			return input.Substring(2);
+		}
+
+		private string GetSeparatorsInner(ref string input)
+		{
+			int endPosi = input.IndexOf(']');
+			var separator = input.Substring(1, endPosi - 1);
+			input = input.Substring(endPosi + 1);
+
+			return separator;
+		}
+
+		private static void ThrowIfContainsNegativeNumbers(IEnumerable<int> numbers)
+		{
+			var negativeNumbers = numbers.Where(x => x < 0);
+			if (negativeNumbers.Any())
+			{
+				var negativesNotAllowed = "negatives not allowed: ";
+				foreach (var nr in negativeNumbers)
+				{
+					negativesNotAllowed = negativesNotAllowed + nr + " ";
+				}
+
+				throw new ArgumentException(negativesNotAllowed);
+			}
+		}
+	}
+}
---
+++ Solo/KataStringCalculator/StringCalcKata2012-08-08/CalculatorTests.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace StringCalcKata2012_08_08
+{
+	[TestFixture]
+    public class CalculatorTests
+    {
+		readonly Calculator sut = new Calculator();
+
+	    [Test]
+	    public void GivenEmpty_Returns0()
+	    {
+		    int result = sut.Add("");
+			Assert.That(0, Is.EqualTo(result));
+	    }
+
+		[Test]
+	    public void GivenNull_Returns0()
+	    {
+		    int result = sut.Add(null);
+			Assert.That(0, Is.EqualTo(result));
+	    }
+
+
+		[Test]
+		public void GivenNr_ReturnsNr()
+		{
+			int result = sut.Add("1");
+			Assert.That(1, Is.EqualTo(result));
+		}
+
+		[Test]
+		public void GivenNewLineInString_TreatAsSeparator()
+		{
+			int result = sut.Add("1\n2");
+			Assert.That(3, Is.EqualTo(result));
+		}
+
+		[Test]
+		public void GivenDashes_SwitchDelimiter()
+		{
+			int result = sut.Add("//;\n1;2");
+			Assert.That(3, Is.EqualTo(result));
+		}
+		
+		[Test]
+		[ExpectedException(typeof(ArgumentException))]
+		public void GivenANegativeNr_Throws()
+		{
+			sut.Add("-1");
+		}
+
+		[Test]
+		[ExpectedException(typeof(ArgumentException), ExpectedMessage = "negatives not allowed: -1 -2 ")]
+		public void GivenMoreNegativeNrs_Throws_ExpectMessage()
+		{
+			sut.Add("-1,-2");
+		}
+
+		[Test]
+		public void GivenNrBiggerThan1000_IgnoreThem()
+		{
+			int result = sut.Add("1000,1");
+			Assert.That(1, Is.EqualTo(result));
+		}
+
+
+		[Test]
+		public void DelimiterOfAnyLenght_SwitchDelimiter()
+		{
+			int result = sut.Add("//[***]\n1***2***3");
+			Assert.That(6, Is.EqualTo(result));
+		}
+
+		[Test]
+		public void MultipleDelimiters_SwitchDelimiter()
+		{
+			int result = sut.Add("//[*][%]\n1*2%3");
+			Assert.That(6, Is.EqualTo(result));
+		}
+
+		[Test]
+		public void MultipleDelimiterOfAnyLenght_SwitchDelimiter()
+		{
+			int result = sut.Add("//[***][%%%]\n1***2%%%3");
+			Assert.That(6, Is.EqualTo(result));
+		}
+		
+
+		[Test]
+		[TestCase("1,2", 3)]
+		[TestCase("1,2,3,4,5,6", 21)]
+		[TestCase("//;\n1;2", 3)]
+		public void Given_Expect_DATADRIVEN(string input, int expected)
+		{
+			int result = sut.Add(input);
+			Assert.That(expected, Is.EqualTo(result));
+		}
+
+
+    }
+}
