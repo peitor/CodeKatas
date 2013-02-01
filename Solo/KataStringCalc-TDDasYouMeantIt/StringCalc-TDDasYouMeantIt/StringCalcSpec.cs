70601b45a9a76fbd63d4d17fa4576d31a34d81e8
Peter Gfader <peter@gfader.com>
2013-02-01 07:05:24 +0100

    StringCalc TDD as you meant it. Start

---
+++ Solo/KataStringCalc-TDDasYouMeantIt/StringCalc-TDDasYouMeantIt/StringCalcSpec.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StringCalc_TDDasYouMeantIt
+{
+	[TestClass]
+	public class StringCalcSpec
+	{
+		[TestMethod]
+		public void Given1Number_ReturnSum()
+		{
+			const string input = "1";
+			int result = 0;
+			
+
+			result += int.Parse(input);
+			
+			
+			
+			Assert.AreEqual(1, result);
+		}
+
+		[TestMethod]
+		public void Given2Numbers_ReturnSum()
+		{
+			const string input = "1,2";
+			int result = 0;
+
+
+			var array = input.Split(',');
+			foreach (var s in array)
+			{
+				result += int.Parse(s);
+			}
+			
+		
+			Assert.AreEqual(3, result);
+		}
+	}
+}
