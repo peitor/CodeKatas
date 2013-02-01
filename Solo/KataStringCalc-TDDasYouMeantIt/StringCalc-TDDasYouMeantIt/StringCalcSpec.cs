cb032a0bda50ad62db4e003f2d0809651075628e
Peter Gfader <peter@gfader.com>
2013-02-01 07:08:09 +0100

    refa to Sum method

--- Solo/KataStringCalc-TDDasYouMeantIt/StringCalc-TDDasYouMeantIt/StringCalcSpec.cs
+++ Solo/KataStringCalc-TDDasYouMeantIt/StringCalc-TDDasYouMeantIt/StringCalcSpec.cs
@@ -10,14 +10,15 @@ namespace StringCalc_TDDasYouMeantIt
 		{
 			const string input = "1";
 			int result = 0;
-			
 
-			result += int.Parse(input);
-			
-			
-			
+
+			result = Sum(input, result);
+
+
 			Assert.AreEqual(1, result);
 		}
+
+	
 
 		[TestMethod]
 		public void Given2Numbers_ReturnSum()
@@ -26,14 +27,23 @@ namespace StringCalc_TDDasYouMeantIt
 			int result = 0;
 
 
+			result = Sum(input, result);
+
+			
+		
+			Assert.AreEqual(3, result);
+		}
+
+
+
+		private static int Sum(string input, int result)
+		{
 			var array = input.Split(',');
 			foreach (var s in array)
 			{
 				result += int.Parse(s);
 			}
-			
-		
-			Assert.AreEqual(3, result);
+			return result;
 		}
 	}
 }
